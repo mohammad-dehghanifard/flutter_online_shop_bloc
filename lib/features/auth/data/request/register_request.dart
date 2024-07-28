78ac7ef1e7711861b2036f0eac068ad65d2c0301
mohammad <m.deheghanifard@gmail.com>
2024-07-28 12:35:51 +0330

    completed RegisterRequest class

--- lib/features/auth/data/request/register_request.dart
+++ lib/features/auth/data/request/register_request.dart
@@ -1,11 +1,26 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 
 class RegisterRequest {
-  dynamic image;
+  File? image;
   final TextEditingController fullNameTxt = TextEditingController();
   final TextEditingController phoneTxt = TextEditingController();
   final TextEditingController addressTxt = TextEditingController();
   final TextEditingController postalCodeTxt = TextEditingController();
   double? lat;
   double? lng;
+
+  FormData sendRequest() {
+    return  FormData.fromMap({
+      "image" : MultipartFile.fromFileSync(image!.path),
+      "phone" : phoneTxt.text,
+      "name" : fullNameTxt.text,
+      "address" : addressTxt.text,
+      "postal_code" : postalCodeTxt.text,
+      "lat" : lat,
+      "lng" : lng
+    });
+  }
 }
